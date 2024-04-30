7a4dbe5b8874528bd13975557271f73ecd4e390d
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-30 08:27:31 +0200

    fix contact text

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,8 +4,8 @@ import { FaLinkedin } from "react-icons/fa";
 export const Contact = () => {
   return (
     <>
-      <h2 className="title">Oslovil vás můj profil? Pojďme se potkat </h2>
-      <p className="contact__text">... zjistíme, zdali najdeme ideální souhrnu mezi mými dovednostmi a vašimi potřebami.</p>
+      <h2 className="title">Oslovil vás můj profil? Pojďme se setkat v Praze!</h2>
+      <p className="contact__text">... zjistíme, zdali najdeme ideální souhru mezi mými dovednostmi a vašimi potřebami.</p>
       <div className="contact__container">
         <a href="mailto:vendysacek@seznam.cz" aria-label="Kontaktovat Vendulu" className="contact__button">
           vendysacek@seznam.cz
