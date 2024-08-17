62bf96e98393ac44310268839a085f91a088d14d
Vendula Krajíčková <vendysacek@seznam.cz>
2024-08-17 19:29:19 +0200

    feat: add project superkviz

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,52 +1,68 @@
 export const projectsData = [
   {
     id: 1,
-    year: '11/2022 - 12/2022',
-    name: 'Expo burza Pardubice',
-    skills: 'HTML / Sass / Eleventy',
-    description: 'Web informuje o jednodenní události, u kterého jsem využila znalosti získané z kurzu <a class="link-anchor show-edu" href="#stan-se-koderkou">Staň se kodérkou</a>, na kterém jsem tento web prezentovala jako závěrečný projekt.',
-    linkGH: 'https://github.com/wendykr/ZAVERECNY-PROJEKT-Expo',
-    linkWeb: 'https://expo-pce.netlify.app/',
-    screen: '../project/screen-expo-pce.jpg'
+    year: "11/2022 - 12/2022",
+    name: "Expo burza Pardubice",
+    skills: "HTML / Sass / Eleventy",
+    description:
+      'Web informuje o jednodenní události, u kterého jsem využila znalosti získané z kurzu <a class="link-anchor show-edu" href="#stan-se-koderkou">Staň se kodérkou</a>, na kterém jsem tento web prezentovala jako závěrečný projekt.',
+    linkGH: "https://github.com/wendykr/ZAVERECNY-PROJEKT-Expo",
+    linkWeb: "https://expo-pce.netlify.app/",
+    screen: "../project/screen-expo-pce.jpg",
   },
   {
     id: 2,
-    year: '05/2023 - 09/2023',
-    name: 'Vinařství',
-    skills: 'JavaScript / Sass / Webpack',
-    description: 'Web s tématikou vinařství je projekt založený na Webpacku a mým cílem bylo si na něm procvičit své znalosti z kurzů JavaScript 1 & <a class="link-anchor show-edu" href="#javascript-2">JavaScript 2</a>.',
-    linkGH: 'https://github.com/wendykr/ZAVERECNY-PROJEKT-Winery',
-    linkWeb: 'https://vinarstvi.netlify.app/',
-    screen: '../project/screen-winery.jpg'
+    year: "05/2023 - 09/2023",
+    name: "Vinařství",
+    skills: "JavaScript / Sass / Webpack",
+    description:
+      'Web s tématikou vinařství je projekt založený na Webpacku a mým cílem bylo si na něm procvičit své znalosti z kurzů JavaScript 1 & <a class="link-anchor show-edu" href="#javascript-2">JavaScript 2</a>.',
+    linkGH: "https://github.com/wendykr/ZAVERECNY-PROJEKT-Winery",
+    linkWeb: "https://vinarstvi.netlify.app/",
+    screen: "../project/screen-winery.jpg",
   },
   {
     id: 3,
-    year: '12/2023 - 12/2023',
-    name: 'Filmová databáze',
-    skills: 'React / API / Sass / Vite',
-    description: 'Web s databází filmů založený na Vite a naprogramovaný v Reactu. Cílem tohoto projektu bylo vytvořit filmovou vyhledávací aplikaci s načítáním data z API v rámci Adventní programovací výzvy ReactGirls a Lenovo.',
-    linkGH: 'https://github.com/wendykr/the-movie-db-app',
-    linkWeb: 'https://filmova-databaze.netlify.app/',
-    screen: '../project/screen-filmova-databaze.jpg'
+    year: "12/2023 - 12/2023",
+    name: "Filmová databáze",
+    skills: "React / API / Sass / Vite",
+    description:
+      "Web s databází filmů založený na Vite a naprogramovaný v Reactu. Cílem tohoto projektu bylo vytvořit filmovou vyhledávací aplikaci s načítáním data z API v rámci Adventní programovací výzvy ReactGirls a Lenovo.",
+    linkGH: "https://github.com/wendykr/the-movie-db-app",
+    linkWeb: "https://filmova-databaze.netlify.app/",
+    screen: "../project/screen-filmova-databaze.jpg",
   },
   {
     id: 4,
-    year: '10/2023 - 12/2023',
-    name: 'Words Game',
-    skills: 'React / Supabase / Sass / Vite',
-    description: 'Web na procvičování anglické slovní zásoby založený na Vite a naprogramovaný v Reactu. Cílem tohoto projektu bylo se naučit základní principy Reactu v rámci <a class="link-anchor show-edu" href="#rg-mentoring">ReactGirls Mentoringového programu</a>.',
-    linkGH: 'https://github.com/wendykr/WORDS-GAME',
-    linkWeb: 'https://words-game.netlify.app/',
-    screen: '../project/screen-words-game.jpg'
+    year: "10/2023 - 12/2023",
+    name: "Words Game",
+    skills: "React / Supabase / Sass / Vite",
+    description:
+      'Web na procvičování anglické slovní zásoby založený na Vite a naprogramovaný v Reactu. Cílem tohoto projektu bylo se naučit základní principy Reactu v rámci <a class="link-anchor show-edu" href="#rg-mentoring">ReactGirls Mentoringového programu</a>.',
+    linkGH: "https://github.com/wendykr/WORDS-GAME",
+    linkWeb: "https://words-game.netlify.app/",
+    screen: "../project/screen-words-game.jpg",
   },
   {
     id: 5,
-    year: '04/2024 - 05/2025',
-    name: 'Osobní stránky a portfolio',
-    skills: 'React / TypeScript / Vite',
-    description: 'Web s prezentací mých projektů a dovedností naprogramovaný v Reactu v kombinaci TypeScriptu. Cílem tohoto projektu si bylo procvičit znalosti kombinace Reactu s TypeScriptem.',
-    linkGH: 'https://github.com/wendykr/MY-SITE',
-    linkWeb: 'https://www.vendula-krajickova.cz/',
-    screen: '../project/screen-my-site.jpg'
+    year: "04/2024 - 05/2025",
+    name: "Osobní stránky a portfolio",
+    skills: "React / TypeScript / Vite",
+    description:
+      "Web s prezentací mých projektů a dovedností naprogramovaný v Reactu v kombinaci TypeScriptu. Cílem tohoto projektu si bylo procvičit znalosti kombinace Reactu s TypeScriptem.",
+    linkGH: "https://github.com/wendykr/MY-SITE",
+    linkWeb: "https://www.vendula-krajickova.cz/",
+    screen: "../project/screen-my-site.jpg",
   },
-]
+  {
+    id: 6,
+    year: "05/2024 - 06/2025",
+    name: "Superkvíz",
+    skills: "React / TypeScript / Supabase / Vite",
+    description:
+      "Kvízová aplikace s výběrem odpovědí na otázky založená na Vite a naprogramovaná v Reactu s TypeScriptem. Cílem tohoto projektu bylo naprogramovat připravený statický HTML/CSS návrh na dynamický web s načítáním dat ze Supabase.",
+    linkGH: "https://github.com/wendykr/superkviz-app-ts",
+    linkWeb: "https://superkviz-app.netlify.app/",
+    screen: "../project/screen-superkviz.jpg",
+  },
+];
