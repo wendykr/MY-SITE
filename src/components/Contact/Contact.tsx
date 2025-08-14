3d5f1e1963a2de11fd30299ccf9b2b5e06a72fa8
Vendula Krajíčková <vendysacek@seznam.cz>
2025-08-14 15:25:39 +0200

    #9 - docs: up text contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,16 +4,29 @@ import { FaLinkedin } from "react-icons/fa";
 export const Contact: React.FC = () => {
   return (
     <>
-      <h2 className="title">Oslovil vás můj profil? Pojďme se setkat v Praze!</h2>
-      <p className="contact__text">... zjistíme, zdali najdeme ideální souhru mezi mými dovednostmi a vašimi potřebami.</p>
+      <h2 className="title">
+        Aktuálně nejsem na lovu, ale odvážní mohou zaklepat.
+      </h2>
+      <p className="contact__text">
+        ... zapojení do krátkodobého projektu nebo nové propojení z oboru?
+        Sem s tím!
+      </p>
       <div className="contact__container">
-        <a href="mailto:vendysacek@seznam.cz" aria-label="Kontaktovat Vendulu" className="contact__button">
+        <a
+          href="mailto:vendysacek@seznam.cz"
+          aria-label="Kontaktovat Vendulu"
+          className="contact__button"
+        >
           vendysacek@seznam.cz
         </a>
-        <a href="https://www.linkedin.com/in/vendula-krajickova/" className="contact__button" aria-label="Profil na LinkedInu">
+        <a
+          href="https://www.linkedin.com/in/vendula-krajickova/"
+          className="contact__button"
+          aria-label="Profil na LinkedInu"
+        >
           <FaLinkedin className="contact__icon" /> LinkedIn
         </a>
       </div>
     </>
-  )
+  );
 }
