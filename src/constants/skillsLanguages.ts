0de181e0f85980f630daed3900c092e1ea47bc6f
Vendula Krajíčková <vendysacek@seznam.cz>
2026-03-14 17:16:16 +0100

    feat(#16): refactor skills data structure and improve icon handling

--- src/constants/skillsLanguages.ts
+++ src/constants/skillsLanguages.ts
@@ -1,33 +1,52 @@
+import { FaReact, FaHtml5, FaSass } from "react-icons/fa";
+import { IoLogoCss3 } from "react-icons/io5";
+import {
+  SiJavascript,
+  SiTypescript,
+  SiTailwindcss,
+} from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+
 export const skillsLanguagesData = [
   {
     id: 1,
-    name: 'React',
-    width: 70,
+    name: "React",
+    icon: FaReact,
   },
   {
     id: 2,
     name: 'JavaScript',
-    width: 84,
+    icon: SiJavascript,
   },
   {
     id: 3,
     name: 'TypeScript',
-    width: 25,
+    icon: SiTypescript,
   },
   {
     id: 4,
-    name: 'HTML',
-    width: 100,
+    name: 'Next.js',
+    icon: SiNextdotjs,
   },
   {
     id: 5,
-    name: 'CSS & Sass',
-    width: 91,
+    name: 'HTML',
+    icon: FaHtml5,
+  },
+  {
+    id: 6,
+    name: 'CSS',
+    icon: IoLogoCss3,
+  },
+  {
+    id: 7,
+    name: 'Sass',
+    icon: FaSass,
   },
 
   {
-    id: 6,
+    id: 8,
     name: 'Tailwind',
-    width: 75,
+    icon: SiTailwindcss,
   },
 ]
