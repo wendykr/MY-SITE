d1bc703d0504bc7b3c089ba8d493f34f443bc031
Vendula Krajíčková <vendysacek@seznam.cz>
2026-07-28 05:17:58 +0200

    feature(#27): add skills section to About page and update localization texts

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,55 +1,30 @@
-import { Link } from "react-router-dom";
-import { scroller } from "react-scroll";
 import "./About.scss";
 import { useTranslation } from "react-i18next";
+import { skillGroups } from "../../constants/skills";
+import { Skill } from "../Skill/Skill";
 
 export const About: React.FC = () => {
   const { t } = useTranslation();
-  const handleClick = (to: string) => {
-    scroller.scrollTo(to, {
-      spy: true,
-      smooth: true,
-      offset: 0,
-      duration: 1000,
-    });
-  };
 
   return (
     <section id={t("about.id")} className="about">
       <h2 className="title">{t("about.title")}</h2>
       <h3 className="about__subtitle">{t("about.subtitle.who")}</h3>
       <div className="about__text">
-        <p>
-          {t("about.text.p1.pre")}{" "}
-          <Link
-            className="link-anchor"
-            to={`#${t("skills.id")}`}
-            onClick={() => handleClick(`${t("skills.id")}`)}
-          >
-            {t("about.text.p1.link")}
-          </Link>{" "}
-          {t("about.text.p1.post")}
-        </p>
+        <p>{t("about.text.p1")}</p>
         <p>{t("about.text.p2")}</p>
         <p>{t("about.text.p3")}</p>
-        <p>{t("about.text.p4")}</p>
       </div>
 
-      <h3 className="about__subtitle">{t("about.subtitle.what")}</h3>
-      <div className="about__text">
-        <p>
-          {t("about.text2.p1.pre")}{" "}
-          <Link
-            className="link-anchor"
-            to={`#${t("projects.id")}`}
-            onClick={() => handleClick(`${t("projects.id")}`)}
-          >
-            {t("about.text2.p1.link")}
-          </Link>{" "}
-          {t("about.text2.p1.post")}
-        </p>
-        <p>{t("about.text2.p2")}</p>
-        <p>{t("about.text2.p3")}</p>
+      <h3 className="about__subtitle">{t("about.subtitle.tech")}</h3>
+      <div className="about__skills">
+        {skillGroups.map((group) => (
+          <div className="about__skills--row" key={group.id}>
+            {group.items.map((item) => (
+              <Skill key={item.name} name={item.name} icon={item.icon} />
+            ))}
+          </div>
+        ))}
       </div>
 
       <h3 className="about__subtitle">{t("about.subtitle.traits")}</h3>
